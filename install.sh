c0185fd0dbe3f31c58a30895317ffcbdb9e8e533
caolige <44917075+caolige@users.noreply.github.com>
2019-05-26 21:17:33 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+# 在golang工作目录下执行
+git clone https://github.com/golang/net.git golang.org/x/net
+git clone https://github.com/golang/sys.git golang.org/x/sys
+git clone https://github.com/golang/tools.git golang.org/x/tools
+git clone https://github.com/golang/lint.git golang.org/x/lint
+
+go get -uv github.com/mdempsky/gocode
+go get -uv github.com/uudashr/gopkgs/cmd/gopkgs
+go get -uv github.com/ramya-rao-a/go-outline
+go get -uv github.com/acroca/go-symbols
+go get -uv golang.org/x/tools/cmd/guru
+go get -uv golang.org/x/tools/cmd/gorename
+go get -uv github.com/go-delve/delve/cmd/dlv
+go get -uv github.com/rogpeppe/godef
+go get -uv github.com/sqs/goreturns
+go get -uv golang.org/x/lint/golint
