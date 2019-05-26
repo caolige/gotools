b4960f13ebaa642e4c39cfe0e36ed16631febf65
caolige <44917075+caolige@users.noreply.github.com>
2019-05-26 21:27:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ git clone https://github.com/golang/sys.git golang.org/x/sys
 git clone https://github.com/golang/tools.git golang.org/x/tools
 git clone https://github.com/golang/lint.git golang.org/x/lint
 
-go get -uv github.com/mdempsky/gocode
-go get -uv github.com/uudashr/gopkgs/cmd/gopkgs
-go get -uv github.com/ramya-rao-a/go-outline
-go get -uv github.com/acroca/go-symbols
-go get -uv golang.org/x/tools/cmd/guru
-go get -uv golang.org/x/tools/cmd/gorename
-go get -uv github.com/go-delve/delve/cmd/dlv
-go get -uv github.com/rogpeppe/godef
-go get -uv github.com/sqs/goreturns
-go get -uv golang.org/x/lint/golint
+go get -v github.com/mdempsky/gocode
+go get -v github.com/uudashr/gopkgs/cmd/gopkgs
+go get -v github.com/ramya-rao-a/go-outline
+go get -v github.com/acroca/go-symbols
+go get -v golang.org/x/tools/cmd/guru
+go get -v golang.org/x/tools/cmd/gorename
+go get -v github.com/go-delve/delve/cmd/dlv
+go get -v github.com/rogpeppe/godef
+go get -v github.com/sqs/goreturns
+go get -v golang.org/x/lint/golint
